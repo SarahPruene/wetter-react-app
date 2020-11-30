136319b7c29bd25a9f62412982a34af47cbfac69
SarahPruene <sarah.pruene91@gmail.com>
2020-11-30 15:00:46 +0100

    add style to weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,7 @@
 import React from 'react'
 
+import './App.css'
+
 export default function Weather() {
-  return (
-    <div className='Weather'>
-      
-    </div>
-  )
+  return <div className='Weather'></div>
 }
