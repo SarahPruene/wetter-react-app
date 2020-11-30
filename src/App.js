caf62820c6b8e656ffdbad0cd05a387e1d43766d
SarahPruene <sarah.pruene91@gmail.com>
2020-11-30 14:20:17 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,22 @@ import React from 'react'
 export default function App() {
   return (
     <div>
-      <div className='container'></div>
+      <div className='container'>
+        <footer>
+          This projects was coded by <span>Sarah</span>
+          and is{' '}
+          <a
+            href='https://github.com/SarahPruene/shecodes-react-weather-app'
+            target='_blank'
+          >
+            open-sourced on Github
+          </a>{' '}
+          and{' '}
+          <a href='/' target='_blank'>
+            hosted on Netlify
+          </a>  
+        </footer>
+      </div>
     </div>
   )
 }
