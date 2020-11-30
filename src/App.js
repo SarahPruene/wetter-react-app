35f8802b8c960b7821fc9453dda3d43008bd3f20
SarahPruene <sarah.pruene91@gmail.com>
2020-11-30 13:24:10 +0100

    try first app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css'
 
 function App() {
-  return <div className='App'></div>
+  return <div className='App'>
+    <div className='container'>
+      <h1>Hello World</h1>
+    </div>
+  </div>
 }
 
 export default App
